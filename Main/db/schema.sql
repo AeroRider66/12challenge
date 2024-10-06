800929328f6814840991c53ed3c10a96728023f7
AeroRider <dave.butterworth.ee@gmail.com>
2024-10-05 21:08:18 -0500

    seeds change

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -11,7 +11,6 @@ CREATE TABLE department (
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(225) NOT NULL,
-    salary INT NOT NULL,
     department_id INT NOT NULL
     FOREIGN KEY (department_id)
     REFERENCES department(id)
@@ -19,17 +18,10 @@ CREATE TABLE role (
 
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(225) NOT NULL,
     last_name VARCHAR(225) NOT NULL,
     role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id),
-    manager_id INT,
     FOREIGN KEY (manager_id)
     REFERENCES (employee(id))
-
 );
-
--- //manager_id VARCHAR(225)
-
---     ON DELETE SET NULL
--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,16 +1,48 @@
-INSERT INTO movies (movie_name)
+
+INSERT INTO department (department_name)
 VALUES ('Lion King'),
        ('The Godfather'),
        ('West Side Story'),
        ('Parasite'),
        ('The Wizard of Oz');
 
-INSERT INTO reviews (movie_id, review)
-VALUES (1, 'Zazu is underrated. Give that hornbill a sequel!'),
-       (2, 'I''m gonna make him an offer you can''t refuse, watch this movie'),
-       (1, 'Scar is the lion everyone loves to hate'),
-       (3, 'Ten years of ballet and three years of tap to join a gang in this neighborhood'),
-       (5, 'The tin man gave a metallic, hollow performance'),
-       (1, 'Hakuna matata'),
-       (5, 'Those flying monkeys are nightmare fuel!');
+INSERT INTO role (department_name, employee_name)
+VALUES
        
+INSERT INTO employee_name (department_name, role_id)
+VALUES
+
+
+
+Engineering == Engineer == Ferguson
+Finance == Simulation == West
+HR == Manager == Ross
+IT == Analyst == Fowler
+
+
+department_name > employee_name
+role_id > employee_name
+department_name > role_id
+
+-- employee_name
+-- Ferguson
+-- Ross
+-- West
+-- Fowler
+--
+-- department_name
+-- Engineering
+-- Finance
+-- HR
+-- IT
+--
+-- role_id
+-- Engineer
+-- Analyst
+-- Manager
+-- Simulation
+
+-- Adison Morrison	    zvvwaqxi    zufypqphae
+-- Kimberly Allen	    wmjkelnv    aixdoyeyed
+-- Blake Howard	    vktpkxbp    ohcahyagtv
+-- Joyce Hunt	        gkodkrkv    hbkxqhiqpj
